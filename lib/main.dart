a56c93df4732a1a3220637b0995d7916cde49151
Peerawat <manopapapin82@gmail.com>
2023-01-21 18:40:35 +0700

    Co-authored-by: NACHANON NUANPHET <nachanon.nuanphet@mail.kmutt.ac.th>

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
+      initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginPage(),
         '/home': (context) => const MyHomePage(),
--- lib/screen/loginPage.dart
+++ lib/screen/loginPage.dart
@@ -53,10 +53,13 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final double _screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
+      backgroundColor: primaryLighterColor,
       appBar: AppBar(
         elevation: 0.0,
-        backgroundColor: Colors.transparent,
+        backgroundColor: primaryLighterColor,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios, color: primaryColor),
           onPressed: () {
@@ -65,115 +68,140 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 40),
+                    child: Image.asset('assets/images/CourseZ_logo.png'),
+                  ),
+                  const Heading20px(
+                    text: 'ยินดีต้อนรับ',
+                  ),
+                  const Heading20px(text: 'โปรดเข้าสู่ระบบเพื่อใช้งาน'),
+                ],
+              ),
+            ),
+            Stack(children: [
+              Container(
+                height: _screenHeight * 0.51,
+                decoration: BoxDecoration(
+                    color: whiteColor,
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(30),
+                        topLeft: Radius.circular(30)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: blackColor.withOpacity(0.4),
+                          spreadRadius: 5,
+                          blurRadius: 7)
+                    ]),
+              ),
               Padding(
-                padding: const EdgeInsets.only(bottom: 40),
-                child: Image.asset('assets/images/CourseZ_logo.jpg'),
-              ),
-              const Heading20px(
-                text: 'ยินดีต้อนรับ',
-                color: primaryColor,
-              ),
-              const Heading20px(text: 'โปรดเข้าสู่ระบบเพื่อใช้งาน'),
-              Form(
-                  key: formKey,
-                  child: Column(children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.only(bottom: 5),
-                            alignment: Alignment.centerLeft,
-                            child: const Heading20px(text: 'อีเมล'),
-                          ),
-                          TextFormField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(40))),
-                                hintText: 'p@example.com',
-                              ),
-                              controller: emailController,
-                              keyboardType: TextInputType.emailAddress,
-                              validator: RequiredValidator(
-                                  errorText: 'อีเมลไม่ถูกต้อง')),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.only(bottom: 5),
-                            alignment: Alignment.centerLeft,
-                            child: const Heading20px(text: 'รหัสผ่าน'),
-                          ),
-                          TextFormField(
-                              cursorColor: primaryColor,
-                              decoration: const InputDecoration(
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(40))),
-                                  hintText: 'รหัสผ่าน',
-                                  suffixIcon:
-                                      Icon(Icons.visibility_off_outlined)),
-                              controller: passwordController,
-                              keyboardType: TextInputType.visiblePassword,
-                              obscureText: true,
-                              validator: RequiredValidator(
-                                  errorText: 'รหัสผ่านไม่ถูกต้อง')),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: !isEmailEmpty && !isPasswordEmpty
-                              ? () async {
-                                  await authController?.loginUser(
-                                      emailController.text,
-                                      passwordController.text);
-                                }
-                              : null,
-                          style: ElevatedButton.styleFrom(
-                            disabledBackgroundColor: primaryLighterColor,
-                            backgroundColor: primaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: [
+                    Form(
+                        key: formKey,
+                        child: Column(children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.only(bottom: 5),
+                                  alignment: Alignment.centerLeft,
+                                  child: const Heading20px(text: 'อีเมล'),
+                                ),
+                                TextFormField(
+                                    decoration: const InputDecoration(
+                                      border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(40))),
+                                      hintText: 'p@example.com',
+                                    ),
+                                    controller: emailController,
+                                    keyboardType: TextInputType.emailAddress,
+                                    validator: RequiredValidator(
+                                        errorText: 'อีเมลไม่ถูกต้อง')),
+                              ],
                             ),
                           ),
-                          child: const Title16px(
-                            text: 'เข้าสู่ระบบ',
-                            color: whiteColor,
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.only(bottom: 5),
+                                  alignment: Alignment.centerLeft,
+                                  child: const Heading20px(text: 'รหัสผ่าน'),
+                                ),
+                                TextFormField(
+                                    cursorColor: primaryColor,
+                                    decoration: const InputDecoration(
+                                        border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(40))),
+                                        hintText: 'รหัสผ่าน',
+                                        suffixIcon: Icon(
+                                            Icons.visibility_off_outlined)),
+                                    controller: passwordController,
+                                    keyboardType: TextInputType.visiblePassword,
+                                    obscureText: true,
+                                    validator: RequiredValidator(
+                                        errorText: 'รหัสผ่านไม่ถูกต้อง')),
+                              ],
+                            ),
                           ),
-                        ),
-                      ),
-                    )
-                    // Bt(text: 'เข้าสู่ระบบ', color: primaryColor),
-                  ])),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const Registerpage();
-                    }));
-                  },
-                  child: const Title16px(
-                    text: 'ลงทะเบียน',
-                    color: primaryColor,
-                  ))
-            ],
-          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: SizedBox(
+                              width: double.infinity,
+                              height: 50,
+                              child: ElevatedButton(
+                                onPressed: !isEmailEmpty && !isPasswordEmpty
+                                    ? () async {
+                                        await authController?.loginUser(
+                                            emailController.text,
+                                            passwordController.text);
+                                      }
+                                    : null,
+                                style: ElevatedButton.styleFrom(
+                                  disabledBackgroundColor: primaryLighterColor,
+                                  backgroundColor: primaryColor,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                ),
+                                child: const Title16px(
+                                  text: 'เข้าสู่ระบบ',
+                                  color: whiteColor,
+                                ),
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return const Registerpage();
+                                }));
+                              },
+                              child: const Title16px(
+                                text: 'ลงทะเบียน',
+                                color: primaryColor,
+                              ))
+                        ])),
+                  ],
+                ),
+              )
+            ])
+          ],
         ),
       ),
     );
