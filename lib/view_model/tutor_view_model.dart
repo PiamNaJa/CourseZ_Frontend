3cc6733234d096e8b8ae4eb127c25d1072690c45
PiamNaJa <peumza1150@gmail.com>
2023-01-30 17:06:27 +0700

    DotsIndicator position fix

--- lib/view_model/tutor_view_model.dart
+++ lib/view_model/tutor_view_model.dart
@@ -1,20 +1,15 @@
-import 'package:coursez/model/user.dart';
 import 'package:coursez/utils/fetchData.dart';
-import 'package:flutter/material.dart';
 
 class TutorViewModel {
   Future<List<dynamic>> loadTutor(int level) async {
     List<dynamic> tutorLevel = [];
     if (level != 0) {
       final t = await fecthData('user/teacher/class/$level');
-      debugPrint(t.toString());
       t.map((e) => tutorLevel.add(e)).toList();
-      
     } else {
       final t = await fecthData('user/teacher');
       t.forEach((e) => tutorLevel.add(e));
     }
-
     return tutorLevel;
   }
 }
