af79f21d316b4559d12e117825b87da718c57685
palmKumpanat <kumpanat.joompabuth>
2023-03-16 20:33:58 +0700

    Review Video Page

--- lib/view_model/video_view_model.dart
+++ lib/view_model/video_view_model.dart
@@ -1,12 +1,21 @@
 import 'package:coursez/model/course.dart';
+import 'package:coursez/model/reviewVideo.dart';
 import 'package:coursez/model/user.dart';
 import 'package:coursez/model/userTeacher.dart';
 import 'package:coursez/model/video.dart';
+import 'package:coursez/repository/review_repository.dart';
 import 'package:coursez/utils/fetchData.dart';
 import 'package:coursez/view_model/date_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:jiffy/jiffy.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class VideoViewModel {
+  final ReviewRepository _reviewRepository = ReviewRepository();
+
+  get whiteColor => null;
   Future<Video> loadVideoById(String courseid, String videoid) async {
     final v = await fecthData("course/$courseid/video/$videoid");
 
@@ -75,5 +84,23 @@ class VideoViewModel {
     timeago = timeago.replaceAll(' ago', 'ที่แล้ว');
 
     return timeago;
+  }
+
+  Future<void> createReviewVideo(
+      String videoId, double rating, String comment) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token')!;
+    final bool isPass = await _reviewRepository.createReviewVideo(
+        videoId, rating, comment, token);
+    if (!isPass) {
+      Get.snackbar('ผิดพลาด', 'มีบางอย่างผิดพลาด',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: whiteColor);
+    } else {
+      Get.back();
+      Get.back();
+      Get.back();
+    }
   }
 }
