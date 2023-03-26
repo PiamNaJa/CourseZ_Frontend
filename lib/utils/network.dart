ef96f51dd072caf03a7f74990117ca1df0c61749
Nachanon <98012263+PiamNaJa@users.noreply.github.com>
2023-03-26 19:59:06 +0700

    chat init

--- lib/utils/network.dart
+++ lib/utils/network.dart
@@ -1,6 +1,6 @@
 class Network {
-  static const String baseUrl = 'http://10.0.2.2:6000';
-  static const String url = '10.0.2.2:6000';
-  // static const String baseUrl = 'https://coursez-backend.onrender.com';
-  // static const String url = 'coursez-backend.onrender.com';
+  // static const String baseUrl = 'http://10.0.2.2:6000';
+  // static const String url = '10.0.2.2:6000';
+  static const String baseUrl = 'https://coursez-backend.onrender.com';
+  static const String url = 'coursez-backend.onrender.com';
 }
